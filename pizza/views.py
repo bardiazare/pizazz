78fb24f439f829531ef87bcf93b4c702c576b6b6
bardiazare <bzarezaman@gmail.com>
2024-07-06 12:50:33 +0330

    front need

--- pizza/views.py
+++ pizza/views.py
@@ -3,9 +3,14 @@ from rest_framework.views import Response
 from rest_framework import status
 from .models import pizza
 from .serializer import pizzaserializer
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
 
 # Create your views here.
+@api_view(['GET'])
 
-def pizza_list():
+def pizza_list(req):
     data = pizza.objects.all()
     serializer = pizzaserializer(data,many=True)
+    return JsonResponse(serializer.data,status=status.HTTP_200_OK,safe=False)
+
--- pizzazz/urls.py
+++ pizzazz/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from pizza import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pizza/',views.pizza_list)
 ]
